98c48d726d87bf61f4d2a87bd2e43a15983577dc
Aleksandr Semenov <semenlinux@gmail.com>
2021-01-12 22:29:06 +0300

    Create README.md and refactoring.

--- download.py
+++ download.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import sys
 from pathlib import Path
 from urllib.parse import urljoin
 
@@ -8,13 +10,26 @@ from pathvalidate import sanitize_filename, sanitize_filepath
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 
-def get_data_from_url(url):
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    response = requests.get(url, verify=False, allow_redirects=False)
-    if response.status_code == 302 or response.raise_for_status():
-        return None
-    return response
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "start_id", type=int, nargs="?", default=1, help="Стартовый индекс"
+    )
+    parser.add_argument(
+        "end_id", type=int, nargs="?", default=10, help="Последний индекс"
+    )
+    return parser
 
+
+def get_data_from_url(url):
+    try:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        response = requests.get(url, verify=False, allow_redirects=False)
+        if response.status_code == 302 or response.raise_for_status():
+            return None
+        return response
+    except:
+        raise
 
 def create_directory(name):
     directory_path = f"./{ name }"
@@ -35,8 +50,8 @@ def write_cover_to_file(data, full_path):
     return full_path if os.path.exists(full_path) else None
 
 
-def parsing_data(text):
-    soup = BeautifulSoup(text, "lxml")
+def parse_book_page(html_content):
+    soup = BeautifulSoup(html_content, "lxml")
     title_tag = soup.find("body").find("div", id="content").find("h1")
     cover_tag = soup.find("body").find("div", class_="bookimage").find("img")["src"]
     comments_tag = (
@@ -102,25 +117,40 @@ def download_cover(url, filename, folder="images/"):
 
 
 def download_description(url):
-    book_data = get_data_from_url(url)
+    try:
+        book_data = get_data_from_url(url)
+    except:
+        raise
     if book_data:
-        book_description = parsing_data(book_data.text)
+        book_description = parse_book_page(book_data.text)
         return book_description
     return None
 
 
 if __name__ == "__main__":
-    for id in range(1, 11):
+    parser = createParser()
+    namespace = parser.parse_args()
+    start_id = namespace.start_id
+    end_id = namespace.end_id + 1
+    for id in range(start_id, end_id):
         url_book_text = f"https://tululu.org/txt.php?id={id}"
         url_book_description = f"https://tululu.org/b{id}/"
-        book_description = download_description(url_book_description)
+        try:
+            book_description = download_description(url_book_description)
+        except:
+            print("Что-то пошло не так:( Проверьте подключение к интернету!")
+            break
         if book_description:
             txt_name = book_description.get("heading")
             cover_url = book_description.get("img")
             genre = book_description.get("genre")
             comments = book_description.get("comments")
-            cover_filename = f"{ id }.{ cover_url.split('.')[-1] }"
+            cover_filename = f"{ id }.{ txt_name }.{ cover_url.split('.')[-1] }"
             txt_filename = f"{ id }.{ txt_name }"
             book_path = download_txt(url_book_text, txt_filename)
+            if not book_path:
+                book_path = "Книга в формате txt отсутвует!"
             cover_path = download_cover(cover_url, cover_filename)
-            print(id, "Заголовок:", txt_name, genre)
+            print(
+                f"Индекс: { id }\nНазвание: { txt_name }\nАвтор: { book_description.get('author') }\nОбложка: {cover_path} \nФайл: { book_path }\n\n"
+            )
